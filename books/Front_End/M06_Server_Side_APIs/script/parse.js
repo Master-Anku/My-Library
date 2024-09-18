215921e6e719d0a362d423a5ad503814603f26ed
Master-Anku <ankumaster1@gmail.com>
2024-09-18 13:15:25 +0630

    github repo

--- books/Front_End/M06_Server_Side_APIs/script/parse.js
+++ books/Front_End/M06_Server_Side_APIs/script/parse.js
@@ -1,13 +1,20 @@
 var requestUrl = 'https://api.github.com/repos/twitter/chill/issues?per_page=5';
 // console.log(requestUrl)
 fetch(requestUrl)
-  .then(function (response) {
-    return response.json();
-  })
+  // .then(function (response) {
+  //   return response.json();
+  // })
+
+  .then((response)=> response.json())
+  
   .then(function (data) {
-    console.log('Github Repo Issues \n----------');
+    console.log(data)
+    console.log('Github Repo Issues \n------------->');
     for (var i = 0; i < data.length; i++) {
       console.log(data[i].url);
       console.log(data[i].user.login);
+      console.log("")
     }
-  });
+  })
+
+  
