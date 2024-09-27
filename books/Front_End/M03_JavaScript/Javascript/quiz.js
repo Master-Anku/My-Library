b52a83d3b93b83b746cd0c6b104f0c373c6f8e85
Master-Anku <ankumaster1@gmail.com>
2024-09-27 20:14:21 +0630

    added quiz

---
+++ books/Front_End/M03_JavaScript/Javascript/quiz.js
@@ -0,0 +1,177 @@
+let questions = [
+    {
+      question: "JavaScript ဆိုတာဘာလဲ?",
+      options: ["programming language။", "markup language", "operating system "],
+      answer: "programming language",
+    },
+    {
+      question: "var က  ဘာ (scope) လဲ။",
+      options: ["function-scope", "block-scope", "global-scope"],
+      answer: "global-scope",
+    },
+    {
+      question: "let က ဘာ (scope) လဲ။",
+      options: ["global-scope", "block-scope", "local-scope"],
+      answer: "block-scope",
+    },
+    {
+      question: "scope ဘယ်နှစ်မျိုးလဲ။ ",
+      options: ["၅", "၄", "၆"],
+      answer: "၄",
+    },
+    {
+        question: "စိတ်ဝင်စားတဲ့ သူအတွက် ဘယ်အရာကို ပထမ လေ့လာသင့်လဲ။",
+        options: ["html", "css", "javascrip"],
+        answer: "html",
+      },
+  ];
+  
+  let currentQuestion = 0;
+  let timeLeft = 36;
+  let timer;
+  let score = 0;
+  
+  
+  function startQuiz() {
+    // console.log("hello");
+    const username = document.getElementById("username").value;
+    if (username.trim() === "") {
+      alert("Please enter your name.");
+      return;
+    }
+    // Shuffle questions array
+    questions = shuffleArray(questions);
+    document.getElementById("start-container").style.display = "none";
+    document.getElementById("quiz-container").style.display = "block";
+    displayQuestion();
+    startTimer(); // Start the timer after displaying the first question
+  }
+  
+  
+  
+  function displayQuestion() {
+    const questionContainer = document.getElementById("question");
+    questionContainer.innerHTML = ""; // Clear previous content
+  
+    const questionNumberLine = document.createElement("div");
+    questionNumberLine.textContent = `Question ${currentQuestion + 1}: `;
+    questionNumberLine.style.textAlign = "center"; // Center alignment
+    questionNumberLine.style.color = "maroon"; // Maroon color
+    questionNumberLine.style.position = "relative"; // Set position to relative
+    questionNumberLine.style.paddingBottom = "10px"; // Offset beneath the text
+    questionNumberLine.style.borderBottom = "1px solid black"; // Underline
+  
+    const questionTextLine = document.createElement("div");
+    questionTextLine.textContent = questions[currentQuestion].question;
+    questionTextLine.style.marginTop = "20px";
+    // questionTextLine.style.maxWidth = "500px"; // Limit maximum width to prevent overflow
+  
+    questionContainer.appendChild(questionNumberLine);
+    questionContainer.appendChild(questionTextLine);
+  
+    const optionsContainer = document.querySelector(".options");
+    optionsContainer.innerHTML = ""; // Clear previous options
+    const shuffledOptions = shuffleArray(questions[currentQuestion].options);
+    shuffledOptions.forEach((option) => {
+      const optionDiv = document.createElement("div");
+      optionDiv.classList.add("option");
+      optionDiv.innerHTML = `<input type="radio" name="answer" value="${option}"> ${option}`;
+      optionsContainer.appendChild(optionDiv);
+    });
+    document.getElementById("next-question").style.display = "block";
+  }
+  
+  
+  function startTimer() {
+    // console.log("Mdy");
+    timer = setInterval(function () {
+      // console.log("Yangon");
+      if (timeLeft > 0) {
+        timeLeft--;
+        document.getElementById("time").textContent = timeLeft;
+      } else {
+        clearInterval(timer);
+        document.getElementById("time").textContent = "Time's up!";
+        document.querySelectorAll('input[type="radio"]').forEach((input) => {
+          input.disabled = true;
+        });
+        setTimeout(nextQuestion, 2000);
+      }
+    }, 1000);
+  }
+  
+  function checkAnswer() {
+    clearInterval(timer);
+    const selectedAnswer = document.querySelector('input[name="answer"]:checked');
+    if (!selectedAnswer) {
+      document.getElementById("feedback").textContent =
+        "Please select an answer!";
+      return;
+    }
+    const answer = selectedAnswer.value;
+    if (answer === questions[currentQuestion].answer) {
+      score++;
+      document.getElementById("feedback").textContent = "Correct!";
+    } else {
+      document.getElementById(
+        "feedback"
+      ).textContent = `Incorrect. The correct answer is ${questions[currentQuestion].answer}.`;
+    }
+    document.querySelectorAll('input[type="radio"]').forEach((input) => {
+      input.disabled = true;
+    });
+    setTimeout(nextQuestion, 1000);
+  }
+  
+  function nextQuestion() {
+    currentQuestion++;
+    if (currentQuestion < questions.length) {
+      timeLeft = 36;
+      displayQuestion();
+      startTimer();
+      document.getElementById("feedback").textContent = "";
+    } else {
+      showResult();
+    }
+  }
+  
+  function showResult() {
+    document.getElementById("quiz-container").style.display = "none";
+    document.getElementById("result-container").style.display = "block";
+    const username = document.getElementById("username").value;
+    const percentage = (score / questions.length) * 100;
+    let resultText;
+    if (percentage >= 50) {
+      resultText = `<span class="pass">You Pass!</span>`;
+    } else {
+      resultText = `<span class="fail">You Fail!</span>`;
+    }
+    document.getElementById(
+      "result"
+    ).innerHTML = `${username}, you scored ${score} out of ${questions.length}!<br>${resultText}`;
+  }
+  
+  function testAgain() {
+    currentQuestion = 0;
+    timeLeft = 10;
+    score = 0;
+    questions = shuffleArray(questions);
+    document.getElementById("result-container").style.display = "none";
+    document.getElementById("quiz-container").style.display = "block";
+    displayQuestion();
+    startTimer();
+  }
+  
+  // Function to shuffle array
+  function shuffleArray(array) {
+    for (let i = array.length - 1; i > 0; i--) {
+      const j = Math.floor(Math.random() * (i + 1));
+      [array[i], array[j]] = [array[j], array[i]];
+    }
+    return array;
+  }
+  
+  function clearPlaceholder(element) {
+    element.placeholder = "";
+  }
+  
---
+++ books/Front_End/M04_Web_APIs/script/quiz.js
@@ -0,0 +1,177 @@
+let questions = [
+    {
+      question: "JavaScript မှာ const ကို ဘာကြောင့်သုံးရတာလဲ?",
+      options: ["variable တန်ဖိုးကို မပြောင်းလဲချင်လို့", "တန်ဖိုးကို အချိန်မရွေး ပြောင်းလဲဖို့", "loop ထဲမှာ သုံးဖို့ "],
+      answer: "variable တန်ဖိုးကို မပြောင်းလဲချင်လို့",
+    },
+    {
+      question: "looping ဘယ်နှစ်ခုရှိိလဲ။",
+      options: ["၇", "၆", "၅"],
+      answer: "၅",
+    },
+    {
+      question: "ဘယ် loop က လွယ်ကူတာလဲ",
+      options: ["for loop", "while loop", "for in loop"],
+      answer: "for loop",
+    },
+    {
+      question: "javascrip မှာ li ကို အသစ်လုပ်ချင်ရင် ဘယ်လို ရေးရလဲ။",
+      options: ["innerHtml", "textcontent", "queryselctor"],
+      answer: "innerHtml",
+    },
+    {
+        question: "စိတ်ဝင်စားတဲ့ သူအတွက် ဘယ်အရာကို ပထမ လေ့လာသင့်လဲ။",
+        options: ["html", "css", "javascrip"],
+        answer: "html",
+      },
+  ];
+  
+  let currentQuestion = 0;
+  let timeLeft = 36;
+  let timer;
+  let score = 0;
+  
+  
+  function startQuiz() {
+    // console.log("hello");
+    const username = document.getElementById("username").value;
+    if (username.trim() === "") {
+      alert("Please enter your name.");
+      return;
+    }
+    // Shuffle questions array
+    questions = shuffleArray(questions);
+    document.getElementById("start-container").style.display = "none";
+    document.getElementById("quiz-container").style.display = "block";
+    displayQuestion();
+    startTimer(); // Start the timer after displaying the first question
+  }
+  
+  
+  
+  function displayQuestion() {
+    const questionContainer = document.getElementById("question");
+    questionContainer.innerHTML = ""; // Clear previous content
+  
+    const questionNumberLine = document.createElement("div");
+    questionNumberLine.textContent = `Question ${currentQuestion + 1}: `;
+    questionNumberLine.style.textAlign = "center"; // Center alignment
+    questionNumberLine.style.color = "maroon"; // Maroon color
+    questionNumberLine.style.position = "relative"; // Set position to relative
+    questionNumberLine.style.paddingBottom = "10px"; // Offset beneath the text
+    questionNumberLine.style.borderBottom = "1px solid black"; // Underline
+  
+    const questionTextLine = document.createElement("div");
+    questionTextLine.textContent = questions[currentQuestion].question;
+    questionTextLine.style.marginTop = "20px";
+    // questionTextLine.style.maxWidth = "500px"; // Limit maximum width to prevent overflow
+  
+    questionContainer.appendChild(questionNumberLine);
+    questionContainer.appendChild(questionTextLine);
+  
+    const optionsContainer = document.querySelector(".options");
+    optionsContainer.innerHTML = ""; // Clear previous options
+    const shuffledOptions = shuffleArray(questions[currentQuestion].options);
+    shuffledOptions.forEach((option) => {
+      const optionDiv = document.createElement("div");
+      optionDiv.classList.add("option");
+      optionDiv.innerHTML = `<input type="radio" name="answer" value="${option}"> ${option}`;
+      optionsContainer.appendChild(optionDiv);
+    });
+    document.getElementById("next-question").style.display = "block";
+  }
+  
+  
+  function startTimer() {
+    // console.log("Mdy");
+    timer = setInterval(function () {
+      // console.log("Yangon");
+      if (timeLeft > 0) {
+        timeLeft--;
+        document.getElementById("time").textContent = timeLeft;
+      } else {
+        clearInterval(timer);
+        document.getElementById("time").textContent = "Time's up!";
+        document.querySelectorAll('input[type="radio"]').forEach((input) => {
+          input.disabled = true;
+        });
+        setTimeout(nextQuestion, 2000);
+      }
+    }, 1000);
+  }
+  
+  function checkAnswer() {
+    clearInterval(timer);
+    const selectedAnswer = document.querySelector('input[name="answer"]:checked');
+    if (!selectedAnswer) {
+      document.getElementById("feedback").textContent =
+        "Please select an answer!";
+      return;
+    }
+    const answer = selectedAnswer.value;
+    if (answer === questions[currentQuestion].answer) {
+      score++;
+      document.getElementById("feedback").textContent = "Correct!";
+    } else {
+      document.getElementById(
+        "feedback"
+      ).textContent = `Incorrect. The correct answer is ${questions[currentQuestion].answer}.`;
+    }
+    document.querySelectorAll('input[type="radio"]').forEach((input) => {
+      input.disabled = true;
+    });
+    setTimeout(nextQuestion, 1000);
+  }
+  
+  function nextQuestion() {
+    currentQuestion++;
+    if (currentQuestion < questions.length) {
+      timeLeft = 36;
+      displayQuestion();
+      startTimer();
+      document.getElementById("feedback").textContent = "";
+    } else {
+      showResult();
+    }
+  }
+  
+  function showResult() {
+    document.getElementById("quiz-container").style.display = "none";
+    document.getElementById("result-container").style.display = "block";
+    const username = document.getElementById("username").value;
+    const percentage = (score / questions.length) * 100;
+    let resultText;
+    if (percentage >= 50) {
+      resultText = `<span class="pass">You Pass!</span>`;
+    } else {
+      resultText = `<span class="fail">You Fail!</span>`;
+    }
+    document.getElementById(
+      "result"
+    ).innerHTML = `${username}, you scored ${score} out of ${questions.length}!<br>${resultText}`;
+  }
+  
+  function testAgain() {
+    currentQuestion = 0;
+    timeLeft = 10;
+    score = 0;
+    questions = shuffleArray(questions);
+    document.getElementById("result-container").style.display = "none";
+    document.getElementById("quiz-container").style.display = "block";
+    displayQuestion();
+    startTimer();
+  }
+  
+  // Function to shuffle array
+  function shuffleArray(array) {
+    for (let i = array.length - 1; i > 0; i--) {
+      const j = Math.floor(Math.random() * (i + 1));
+      [array[i], array[j]] = [array[j], array[i]];
+    }
+    return array;
+  }
+  
+  function clearPlaceholder(element) {
+    element.placeholder = "";
+  }
+  
